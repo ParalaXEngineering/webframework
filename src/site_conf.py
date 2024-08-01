d98ee2660ce5f142fac601b4e9b53af14b338e46
Matthieu Remacle <matthieu.remacle.ext@fnherstal.com>
2024-08-01 13:29:38 +0200

    BUGFIX: image path

--- src/site_conf.py
+++ src/site_conf.py
@@ -360,4 +360,5 @@ class Site_conf:
     
     def get_statics(self, app_path) -> dict:
         """Function to store the other endpoints that must be registered by the application, for instance to serve images """
-        return {"images": os.path.join(app_path, "website", "assets", "images")}
+        return {"images": os.path.join(app_path, "website", "assets", "images"), 
+                "js": os.path.join(app_path, "website", "assets", "js")}
