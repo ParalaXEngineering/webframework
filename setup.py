663d58a19468bea6baf865b9a17ab800b9425ce8
Matthieu Remacle <matthieu.remacle.ext@fnherstal.com>
2025-11-27 15:48:57 +0100

    REFACT: add a folder for future refactorings

--- setup.py
+++
@@ -1,9 +0,0 @@
-"""Setup configuration for paralax-webframework."""
-
-from setuptools import setup, find_packages
-
-setup(
-    package_dir={"": "."},
-    packages=find_packages(include=["src", "src.*"]),
-    include_package_data=True,
-)
